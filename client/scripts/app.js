d6fae5305f5496f4a140bca0cc72d02277cb701a
ojolao <ojolao@gmail.com>
2017-02-17 15:37:31 -0800

    pass tests except for events

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,43 @@
-// YOUR CODE HERE:
+var app = {
+  server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages'
+};
+app.init = function() { };
+app.send = function (message) {
+  $.ajax({
+    url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+app.fetch = function () {
+  $.ajax({
+    url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+    type: 'GET',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: chats received');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to receive chats', data);
+    }
+  });
+};
+app.clearMessages = function() {
+  $('#chats').empty();
+};
+app.renderMessage = function (message) {
+  $('#chats').append(`<div>${message.text}</div>`);
+};
+app.renderRoom = function (name) {
+  $('#roomSelect').append(`<div>${name}</div>`);
+};
