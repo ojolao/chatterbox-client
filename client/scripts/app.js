b02d776959fe98b4e0842a4476bf82b4c98d6964
ojolao <ojolao@gmail.com>
2017-02-17 16:13:11 -0800

    implement escapehtml for escaping user inputs (user name and message)

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,7 +1,9 @@
 var app = {
   server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages'
 };
-app.init = function() { };
+app.init = function() { 
+  this.fetch();
+};
 app.send = function (message) {
   $.ajax({
     url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
@@ -21,10 +23,15 @@ app.fetch = function () {
   $.ajax({
     url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
     type: 'GET',
-    data: JSON.stringify(message),
     contentType: 'application/json',
     success: function (data) {
       console.log('chatterbox: chats received');
+      console.log(data);
+      var messages = data.results;
+      messages.forEach(function(message) {
+        app.renderMessage(message);
+      });
+      //messages.forEach(this.renderMessage(message));
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -35,9 +42,18 @@ app.fetch = function () {
 app.clearMessages = function() {
   $('#chats').empty();
 };
-app.renderMessage = function (message) {
-  $('#chats').append(`<div>${message.text}</div>`);
+app.renderMessage = function (messageObj) {
+  var escapeHtml = function (str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+  var escapedText = escapeHtml(messageObj.text);
+  var escapedUsername = escapeHtml(messageObj.username);
+  $('#chats').append(`<div>${escapedUsername}: ${escapedText}</div>`);
 };
 app.renderRoom = function (name) {
   $('#roomSelect').append(`<div>${name}</div>`);
 };
+
+app.init();
